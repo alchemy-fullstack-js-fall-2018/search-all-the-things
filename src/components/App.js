016bfad8dd3ccf8a1fae1f109d4c814fab4f914b
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-11-14 17:44:19 -0800

    paging rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Paging from './paging/Paging';
 
 export default class App extends Component {
+  state = {
+    currentPage: 1,
+    totalPages: 1
+  };
+
+  updatePage = page => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
+    const { currentPage, totalPages } = this.state;
     return (
-      <h1>Hello World</h1>
+      <Fragment>
+        <h1>Hello World</h1>
+        <Paging currentPage={currentPage} totalPages={totalPages} updatePage={this.updatePage} />
+      </Fragment>
     );
   }
 }
