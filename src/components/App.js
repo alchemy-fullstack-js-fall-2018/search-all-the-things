b6c977983f8d78dc9e3ed1670060c54fffd585de
Jack <ajackofsometrades@gmail.com>
2018-11-14 16:35:11 -0800

    scaffolding

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+import styles from './App.css';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <Fragment>
+        <h1>AND WE'RE OFF</h1>
+      </Fragment>
+    );
+  }
+}
