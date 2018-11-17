acf883564ac34ba321fdc20844ee4d7822d813b8
KLPainter <KLPainter@gmail.com>
2018-11-17 13:49:23 -0800

    changed to arrow functions

--- src/services/mtgApi.js
+++ src/services/mtgApi.js
@@ -9,21 +9,21 @@ const getResource = (page, resource, query) => {
   let totalResults = 0;
   let totalPages = 0;
 
-  return fetch(myRequest).then(function(response) {
+  return fetch(myRequest).then(response => {
     totalResults = parseInt(response.headers.get('total-count'));
     let pageSize = parseInt(response.headers.get('page-size'));
     totalPages = Math.ceil(totalResults / pageSize);
 
     return response.json();
   })
-    .then(function(json) {
+    .then(json => {
       return {
         totalResults: totalResults,
         totalPages: totalPages,
         results: json[resource]
       };
     })
-    .catch(function(error) { console.log(error);}); /* eslint-disable-line no-console */
+    .catch(error => { console.log(error);}); /* eslint-disable-line no-console */
 };
 
 export const getCards = (page, query) => {
